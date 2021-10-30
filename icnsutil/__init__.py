8c726e03bd804929dc1ab8d440f70f3a72f0408b
relikd <info@relikd.de>
2021-10-30 18:39:52 +0200

    fix pypi-classifiers. Min python version is 3.5

--- icnsutil/__init__.py
+++ icnsutil/__init__.py
@@ -2,7 +2,7 @@
 '''
 A fully-featured python library to handle reading and writing icns files.
 '''
-__version__ = '1.0'
+__version__ = '1.0.1'
 
 from .IcnsFile import IcnsFile
 from .ArgbImage import ArgbImage, PIL_ENABLED
